a0030db3b7d2b0f171cb86d670ce1a569ca20c32
Nikita <ncheusov@inbox.ru>
2022-02-17 22:37:14 +0300

    heroku

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -14,7 +14,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1560,
+        offset: 210,
         charEnded: false
     }
     
